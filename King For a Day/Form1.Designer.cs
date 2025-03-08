056ba7461053bdc3e77959d138e76ba32270030d
unknown <kyibiapino@gmail.com>
2025-03-08 18:21:40 -0300

    projeto inicial rodando

---
+++ King For a Day/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace King_For_a_Day
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtJogadores = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(99, 66);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "button1";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtJogadores
+            // 
+            this.txtJogadores.Location = new System.Drawing.Point(65, 118);
+            this.txtJogadores.Multiline = true;
+            this.txtJogadores.Name = "txtJogadores";
+            this.txtJogadores.Size = new System.Drawing.Size(152, 176);
+            this.txtJogadores.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtJogadores);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtJogadores;
+    }
+}
+
